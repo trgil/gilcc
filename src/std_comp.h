3ed1b3d480c9279f6de84b973ca05e1348f1774e
gil <tkks085@gmail.com>
2019-12-20 01:49:42 -0800

    Add standard-dependent limits struct.

--- src/std_comp.h
+++ src/std_comp.h
@@ -29,4 +29,76 @@
 #define C_STANDARD_C11_GNU  0x0100UL
 #define C_STANDARD_C17_ORIG 0x0200UL
 
+/* Standard dependent limits */
+struct std_trans_lim {
+
+    /* Nesting level for: compound statements, iteration control,
+     * selection control structures.
+     */
+    unsigned int cmpnd_statements_nst_lvl;
+
+    /* Nesting level for conditional inclusion. */
+    unsigned int cond_nst_lvl;
+
+    /* Number of declarators in sequence modifying a type in declaration. */
+    unsigned int decl_mod_num;
+
+    /* Nesting level for parenthesized declarators in a declarator. */
+    unsigned int paren_decl_nst_lvl;
+
+    /* Nesting level for parenthesized expressions in an expression. */
+    unsigned int paren_exp_nst_lvl;
+
+    /* Number of significant initial characters in an internal identifier. */
+    unsigned int init_char_intern_ident_num;
+
+    /* Number of significant initial characters in an external identifier. */
+    unsigned int init_char_extern_ident_num;
+
+    /* Number of external identifiers in a single translation unit. */
+    unsigned int extern_ident_num;
+
+    /* Number of identifiers in a block scope. */
+    unsigned int ident_block_num;
+
+    /* Number of macro identifiers in a single translation unit. */
+    unsigned int ident_macro_num;
+
+    /* Number of parameters in a function definition. */
+    unsigned int param_func_num;
+
+    /* Number of arguments in a function call. */
+    unsigned int arg_func_num;
+
+    /* Number of parameters in a macro definition. */
+    unsigned int param_macro_num;
+
+    /* Number of arguments in a macro call. */
+    unsigned int arg_macro_num;
+
+    /* Number of characters in a line of source code. */
+    unsigned int char_src_line_num;
+
+    /* Number of characters in a string literal. */
+    unsigned int char_src_str_num;
+
+    /* Number of bytes in a single object. */
+    unsigned int byte_obj_num;
+
+    /* Nesting level for included files. */
+    unsigned int f_incl_nst_lvl;
+
+    /* Number of case labels in a switch statement. */
+    unsigned int cslbl_num;
+
+    /* Number of members in a struct or union. */
+    unsigned int membrs_cmpnd_num;
+
+    /* Number of enumeration constants in an enumeration. */
+    unsigned int enm_cnst_num;
+
+    /* Nesting level for structs and unions. */
+    unsigned int cmpnd_nst_lvl;
+};
+
 #endif /* _STD_COMP_H__ */
